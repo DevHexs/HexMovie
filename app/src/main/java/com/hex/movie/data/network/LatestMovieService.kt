2e03ca83ff2a3a8f209909fbefaf14afe9194b6e
Gilberto Gomez <gilbertog@ilikeit.app>
2023-02-28 21:58:14 -0500

    Fix: Dependencies for LatestMovieService.kt

--- app/src/main/java/com/hex/movie/data/network/LatestMovieService.kt
+++ app/src/main/java/com/hex/movie/data/network/LatestMovieService.kt
@@ -1,15 +1,16 @@
 package com.hex.movie.data.network
 
 import com.hex.movie.data.model.MovieLatestData
+import com.hex.movie.data.repo.LatestMovieRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class LatestMovieService @Inject constructor(private val api: MovieApiClient) {
+class LatestMovieService @Inject constructor(private val latestMovieRepository: LatestMovieRepository) {
     suspend fun getLatestMovie(): MovieLatestData{
         return withContext(Dispatchers.IO){
-            val response = api.getLatestMovie()
-            response.body() ?: MovieLatestData()
+            val response = latestMovieRepository.getMovieLatest()
+            response
         }
     }
 }
